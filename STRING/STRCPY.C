593fbe4eafae1595380dd409e26b7e4781de4261
Ellen Körbes <ellenkorbes@gmail.com>
2021-08-15 00:19:57 +0200

    Update STRCPY.C

--- STRING/STRCPY.C
+++ STRING/STRCPY.C
@@ -12,8 +12,7 @@ char *(strcpy)(char *dest, const char *src) {
     char *temp = dest;
 	while (*src != '\0') {
 	    *temp = *src;
-	    temp++;
-	    src++;
+	    temp++, src++;
 	};
 	*temp = '\0';
 	return (dest);
