f0cd2a1f26e006ff8a413232087b7ad4dddd256a
Ellen Körbes <ellenkorbes@gmail.com>
2021-08-15 00:19:41 +0200

    Update MEMCPY.C

--- STRING/MEMCPY.C
+++ STRING/MEMCPY.C
@@ -1,1 +1,18 @@
+#include <string.h>
 
+// c89 definition:
+//
+// Description
+// The memcpy function copies n characters from the object pointed to by src into the object pointed to by dest. If copying takes place between objects that overlap, the behavior is undefined. 
+// Returns
+// The memcpy function returns the value of dest. 
+
+void *(memcpy)(void *dest, const void *src, size_t size) {
+	char *tempDest = (char *)dest;
+	char *tempSrc = (char *)src;
+	while (size > 0) {
+	    *tempDest = *tempSrc;
+	    tempDest++, tempSrc++, size--;
+	};
+	return (dest);
+}
